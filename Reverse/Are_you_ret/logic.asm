033a0dc62a1989b0b442a23faa80c3ce0923ec1d
tuxnode <tuxnode@outlook.com>
2026-05-28 10:03:52 +0800

    feat: Reverse ret new logic

--- Reverse/Are_you_ret/logic.asm
+++ Reverse/Are_you_ret/logic.asm
@@ -1,13 +1,13 @@
 ; 编译命令: nasm -f elf64 logic.asm -o logic.o
 section .text
     global secure_check        ; 导出函数给 C 调用
-    extern check_flag          ; 导入 C 语言定义的逻辑
+    extern xor_flag          ; 导入 C 语言定义的逻辑
 
 secure_check:
     push rbp
     mov rbp, rsp
 
-    mov rax, check_flag
+    mov rax, xor_flag
     pop rbp
     push rax
     ret
--- Reverse/Are_you_ret/main.c
+++ Reverse/Are_you_ret/main.c
@@ -2,36 +2,60 @@
 #include <string.h>
 #include <stdbool.h>
 
-// 声明汇编中的外部函数
+extern void xor_flag(const char* input);
 extern bool secure_check(const char* input);
 
 unsigned char encrypted_flag[] = {
-    0x15, 0x09, 0x02, 0x15, 0x1e, 0x2c, 0x6f, 0x39,
-    0x3a, 0x10, 0x2c, 0x6d, 0x76, 0x47, 0x58, 0x0e
+    0x71, 0x7b, 0x67, 0x61, 0x41, 0x43, 0x0d, 0x5f,
+    0x4f, 0x4f, 0x1e, 0x5d, 0x44, 0x71, 0x3c, 0x7c
 };
-const char* key = "secret_key_2026";
+const char* key1 = "dret_obfu_2026";
+const char* key2 = "secret_key_2026";
 
-// 这个函数会被汇编代码在“假栈”上调用
 bool check_flag(const char* input) {
-    // __asm__ __volatile__ (
-    // ".intel_syntax noprefix;" // 切换到 Intel 语法
-    // "sub rsp, 8;"             // 调整栈指针
-    // ".att_syntax prefix;"     // 切换回 AT&T 语法（编译器默认）
-    // :                         // 无输出
-    // :                         // 无输入
-    // : "rsp"                   // 告诉编译器 rsp 被修改了（Clobber list）
-    // );
+    __asm__ __volatile__(
+    ".intel_syntax noprefix;"
+    "sub rsp, 8;"
+    ".att_syntax prefix;"
+    :
+    :
+    : "rsp"
+    );
     int flag_len = sizeof(encrypted_flag);
-    int key_len = strlen(key);
+    int key_len = strlen(key2);
 
     if (strlen(input) != flag_len) return false;
 
     for (int i = 0; i < flag_len; i++) {
-        if ((unsigned char)(input[i] ^ key[i % key_len]) != encrypted_flag[i]) {
+        if ((unsigned char)(input[i] ^ key2[i % key_len]) != encrypted_flag[i]) {
             return false;
         }
     }
     return true;
+}
+
+void xor_flag(const char* input) {
+    static char decrypted[64];
+    int len = strlen(input);
+    int key1_len = strlen(key1);
+
+    for (int i = 0; i < len; i++) {
+        decrypted[i] = input[i] ^ key1[i % key1_len];
+    }
+    decrypted[len] = '\0';
+
+    __asm__ __volatile__(
+        ".intel_syntax noprefix;"
+        "leave;"
+        "mov rdi, %[buf];"
+        "push %[addr];"
+        "ret;"
+        ".att_syntax prefix;"
+        :
+        : [buf] "r"(decrypted), [addr] "r"(check_flag)
+        : "rdi"
+    );
+    __builtin_unreachable();
 }
 
 int main() {
@@ -39,7 +63,6 @@ int main() {
     printf("请输入 Flag: ");
     if (scanf("%63s", input) != 1) return 1;
 
-    // 调用汇编入口，而非直接调用 check_flag
     if (secure_check(input)) {
         printf("恭喜！Flag 正确。\n");
     } else {
