eb99611d4e5d0173c3f353466a9b4c7f2d235aec
tuxnode <tuxnode@outlook.com>
2026-06-04 21:33:48 +0800

    feat: add Web rce

--- Web/rce/index.php
+++ Web/rce/index.php
@@ -1,20 +1,10 @@
 <?php
-    highlight_file(__FILE__);
-
-    $target = $_GET['ip'];
-
-    if(isset($target)){
-        $blacklist = array(" ", "ls", "cat", "flag");
-        foreach($blacklist as $word){
-            if(preg_match("/$word/", $target)){
-                die("Hack detected! No '$word' allowed.");
-            }
-        }
-
-        // 执行命令
-        // 原意：ping -c 4 127.0.0.1
-        echo "<pre>";
-        system("ping -c 4 " . $target);
-        echo "</pre>";
-    }
+error_reporting(0);
+$cmd = $_REQUEST['cmd'] ?? '';
+if ($cmd !== '') {
+    shell_exec($cmd);
+    echo "Command executed.";
+} else {
+    show_source(__FILE__);
+}
 ?>
